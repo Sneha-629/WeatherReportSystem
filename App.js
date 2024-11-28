3b76feae2600de4b51d58656e4f78e13116f8f4e
Sneha-629 <snehanagaraj796@gmail.com>
2024-11-28 18:44:04 +0530

    App.js  file added

---
+++ App.js
@@ -0,0 +1,14 @@
+import Heading from "./Heading";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Heading/>
+    <Weather/>
+    </div>
+  );
+}
+
+export default App;
