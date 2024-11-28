4414572fc54401d6340e8d3225a577532398eedb
Sneha-629 <snehanagaraj796@gmail.com>
2024-11-28 18:34:06 +0530

    Weather Report System Files uploaded

---
+++ Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './heading.css'
+function Heading() {
+  return (
+    <>
+    <h1 className='heading'>Weather Report Application</h1>
+    </>
+  )
+}
+
+export default Heading
---
+++ Weather.js
@@ -0,0 +1,71 @@
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+import './weather.css'
+
+const APIKey='ksj2xeXELnJ9wi84bRJxUQ4dRieldHqe'
+function Weather() {
+    const[city,setCity]=useState("")
+    const[weatherdata,setWeatherdata]=useState(null)
+    const[error,setError]=useState(null)
+
+    useEffect(() => {
+      fetchWeatherByGeolocation();
+    }, []);
+  
+    const fetchWeatherByGeolocation = async () => {
+      try {
+    // Get user's current position using Geolocation API
+navigator.geolocation.getCurrentPosition(async (position) => {
+  const { latitude, longitude } = position.coords;
+     const response = await axios.get(
+`https://api.tomorrow.io/v4/weather/realtime?location=${latitude},${longitude}&apikey=${APIKey}`
+          );
+          console.log(response.data)
+          setWeatherdata(response.data);
+          setError(null);
+        });
+      } catch (error) {
+        setError('Failed to fetch weather data');
+        setWeatherdata(null);
+      }
+    };
+  
+
+    const handleSubmit=async () => {
+        try {
+          const response = await axios.get(
+ `https://api.tomorrow.io/v4/weather/realtime?location=${city}&apikey=${APIKey}`
+          );
+          setWeatherdata(response.data);
+          setError(null);
+        } catch (error) {
+          setError('Failed to fetch weather data');
+          setWeatherdata(null);
+        }
+      };
+    console.log(weatherdata)
+    // console.log(error)
+  return (
+    <>
+  <div className='container'>
+<h1 className='title'>Search Weather Condition</h1>
+<div className='inputContainer'>
+<input type='text' placeholder='Enter City Name'
+ className='input'
+ onChange={(e)=>setCity(e.target.value)}/>
+<button className='button' onClick={handleSubmit}>Search</button>
+</div>
+{error&&<p className='error'>{error}</p>}
+{weatherdata&&(
+    <div className='weatherContainer'>
+        <h2 className='subtitle'>{weatherdata.location.name}</h2>
+ <p className='temperature'>Temperature:{weatherdata.data.values.temperature}<sup>o</sup>C</p>
+        <p className='humidity'>Humidity:{weatherdata.data.values.humidity}%</p>
+        <p className='windspeed'>Wind Speed:{weatherdata.data.values.windSpeed}mph</p>
+    </div>
+)}
+  </div>
+    </>
+  )
+}
+export default Weather
